4383fca5c93433eb769c857ea2fdde3a3603554a
sushil <55785163+sushilkeer9977@users.noreply.github.com>
2022-09-30 15:14:39 +0530

    add file on strcture

---
+++ structure/int_print_strct.c
@@ -0,0 +1,12 @@
+//Define a structure with 3 members: sid, sname, avrg Initialize it & print it:
+#include<stdio.h>
+{
+    struct student s1;
+    int stuid;
+    char section;
+    float avrg;
+}
+main()
+{
+    
+}
